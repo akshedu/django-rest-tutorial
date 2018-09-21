6d162697c92c61eddfc6221ca5e4b3e71232b7bd
Akshansh Gupta <akshedu@gmail.com>
2018-09-21 13:08:49 +0530

    django rest framework tutorial

---
+++ snippets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from snippets import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('snippets', views.SnippetViewSet)
+router.register('users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls),)
+]
